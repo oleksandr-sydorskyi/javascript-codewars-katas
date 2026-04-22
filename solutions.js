f7c4641521de504a96462cd35d5ca4d298e95c3e
Oleksandr Sydorskyi <88611124+oleksandr-sydorskyi@users.noreply.github.com>
2026-04-22 16:51:52 +0300

    Add some tasks

--- solutions.js
+++ solutions.js
@@ -58,3 +58,34 @@ function min(arr, toReturn) {
   let min = Math.min(...arr);
   return toReturn === "value" ? min : arr.indexOf(min);
 }
+
+// You Can't Code Under Pressure #1
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// Twice as old
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - 2 * sonYearsOld);
+}
+
+// Get Nth Even Number
+function nthEven(n) {
+  return (n - 1) * 2;
+}
+
+// What's the real floor?
+function getRealFloor(n) {
+  if (n <= 0) return n;
+  return n <= 13 ? n - 1 : n - 2;
+}
+
+// Beginner Series #2 Clock
+function past(h, m, s) {
+  return h * 3600000 + m * 60000 + s * 1000;
+}
+
+// Is n divisible by x and y?
+function isDivisible(n, x, y) {
+  return n % x === 0 && n % y === 0;
+}
